50f5c25970b9caecf5a3ce9477a60fba0bdc0723
tsiguenz <tsiguenz@student.42.fr>
2024-02-08 14:12:21 +0100

    use ansible_host to set the WP_URL, add informations and colors

--- set_env.sh
+++ set_env.sh
@@ -1,21 +1,38 @@
 #!/bin/bash
 
-set -eu
+set -u
 
+GREEN="\e[32m"
+RED="\e[31m"
+LIGHT_RED="\e[91m"
+DEFAULT="\e[0m"
 ENV_FILE=srcs/.env
 INVENTORY=inventory.yml
+ANSIBLE_HOST_VAR_NAME=ansible_host
+
+function print_info() {
+	echo -e "$GREEN""[INFO]: ""$1""$DEFAULT"
+}
+
+function print_warning() {
+	echo -e "$LIGHT_RED""[WARN]: ""$1""$DEFAULT"
+}
+
+function print_error() {
+	echo -e "$RED""[ERROR]: ""$1""$DEFAULT"
+}
 
 function create_env_file() {
 	if [ ! -f $ENV_FILE ]; then
-		echo "File ${ENV_FILE} does not exist. Creating it..."
+		print_info "File ${ENV_FILE} does not exist. Creating it..."
 	else
 		echo "File ${ENV_FILE} exists, do you want to overwrite it? (y/n)"
 		read -r OVERWRITE
 		if [ "${OVERWRITE}" = "y" ]; then
-			echo "Overwriting ${ENV_FILE}..."
+			print_info "Overwriting ${ENV_FILE}..."
 			rm -f ${ENV_FILE}
 		else
-			echo "Not overwriting ${ENV_FILE}"
+			print_info "Not overwriting ${ENV_FILE}"
 			exit 0
 		fi
 	fi
@@ -23,18 +40,32 @@ function create_env_file() {
 }
 
 function set_ansible_host() {
-	if grep "ansible_host" $INVENTORY | sed 's/^\s*//g'; then
-		echo "Host is already set in $INVENTORY"
-		echo "Edit manually if you want to change it."
+	if grep $ANSIBLE_HOST_VAR_NAME $INVENTORY > /dev/null; then
+		print_info "Host is already set in $INVENTORY"
+		print_info "Edit manually if you want to change it."
 		return
 	fi
-	echo "Enter value ansible_host:"
+	echo "Enter value $ANSIBLE_HOST_VAR_NAME:"
 	read -r VALUE
-	echo "      ansible_host: $VALUE" >>$INVENTORY
+	echo "      $ANSIBLE_HOST_VAR_NAME: $VALUE" >> $INVENTORY
+}
+
+function set_wp_url() {
+	local ANSIBLE_HOST_LINE
+	ANSIBLE_HOST_LINE=$(grep $ANSIBLE_HOST_VAR_NAME $INVENTORY)
+	if [ $? -ne 0 ]; then
+		print_warning "Can't set WP_URL, $ANSIBLE_HOST_VAR_NAME not set in $INVENTORY..."
+		return
+	fi
+	local HOST=$(echo $ANSIBLE_HOST_LINE \
+				| sed "s/^\s*$ANSIBLE_HOST_VAR_NAME: //g")
+	set_env_var "WP_URL" "https://$HOST/"
+
 }
 
 function set_env_var() {
-	echo "${1}=\"${2}\"" >>${ENV_FILE}
+	echo "${1}=\"${2}\"" >> ${ENV_FILE}
+	print_info "${1}=\"${2}\" added to $ENV_FILE"
 }
 
 function ask_to_set_env_var() {
@@ -44,8 +75,8 @@ function ask_to_set_env_var() {
 }
 
 function generate_random_env_var() {
-	echo "Generating random value for ${1}..."
-	RAND=$(openssl rand -hex 32)
+	print_info "Generating random value for ${1}..."
+	RAND=$(/bin/openssl rand -hex 32)
 	set_env_var "${1}" "${RAND}"
 }
 
@@ -66,7 +97,7 @@ function set_env() {
 	ask_to_set_env_var "WP_USER_MAIL"
 
 	ask_to_set_env_var "WP_TITLE"
-	ask_to_set_env_var "WP_URL"
+	set_wp_url
 
 	generate_random_env_var "WORDPRESS_AUTH_KEY"
 	generate_random_env_var "WORDPRESS_SECURE_AUTH_KEY"
